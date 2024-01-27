22c0f4cd29dd63af1fa33019da15d3b83b6056e2
Merlin Nuñez <merlinsn@gmail.com>
2024-01-27 17:13:09 +0100

    first working commit

--- app/src/main/java/com/example/controller/MainActivity.kt
+++ app/src/main/java/com/example/controller/MainActivity.kt
@@ -1,28 +1,33 @@
 package com.example.controller
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.StrictMode
+import android.view.MotionEvent
+import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.controller.databinding.ActivityMainBinding
 import java.io.IOException
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
-import androidx.appcompat.app.AppCompatActivity
-import com.example.controller.databinding.ActivityMainBinding
 
-var mySpeed: Int = 0
-var myDirection: Int = 0
+
+var speed: Int = 0
+var reverse: Int = 0
+var direction: Int = 0
 var myTargetIP = "not set"
 var myTargetPort = "not set"
-var myUDP: String = ""
+var throttle: Long = 50
 
 private lateinit var binding: ActivityMainBinding
 
 
-public class SoftOptions {
-    var RemoteHost: String = "192.168.0.61"
-    var RemotePort: Int = 1234
+class SoftOptions {
+    var remoteHost: String = "192.168.0.12"
+    var remotePort: Int = 4210
 
     constructor()
 
@@ -34,7 +39,7 @@ val mySettings = SoftOptions()
 
 class MainActivity : AppCompatActivity() {
 
-    fun sendUDP(messageStr: String) {
+    private fun sendUDP(messageStr: String) {
         // Hack Prevent crash (sending should be done using an async task)
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
@@ -46,8 +51,8 @@ class MainActivity : AppCompatActivity() {
             val sendPacket = DatagramPacket(
                 sendData,
                 sendData.size,
-                InetAddress.getByName(mySettings.RemoteHost),
-                mySettings.RemotePort
+                InetAddress.getByName(mySettings.remoteHost),
+                mySettings.remotePort
             )
             socket.send(sendPacket)
         } catch (e: IOException) {
@@ -55,73 +60,84 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun updateControls(){
+        Thread.sleep(throttle) // To avoid overloading the receiver.
+
+        val message = "$direction$speed$reverse"
+        sendUDP(message)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
+        Thread {
+            while (true){updateControls()}
+        }.start()
         val view = binding.root
         setContentView(view)
 
-        var intent = intent
-        val IPtoshow = intent.getStringExtra("myTargetIP")
         val resultTv = this.findViewById<TextView>(R.id.txtShowIP)
-        resultTv.text = "Target device IP is set as " + mySettings.RemoteHost
-        val PortToShow = intent.getStringExtra("myTargetPort")
+        resultTv.text = "Target device IP is set as " + mySettings.remoteHost
         val resultTvPort = this.findViewById<TextView>(R.id.txtShowPort)
-        resultTvPort.text = "Target device port is set as " + mySettings.RemotePort
+        resultTvPort.text = "Target device port is set as " + mySettings.remotePort
+        val speedText = this.findViewById<TextView>(R.id.speedView)
 
-        binding.btnIncrease.setOnClickListener {
-            mySpeed += 1
-            if (mySpeed > 10) mySpeed = 10
-            val SpeedtoShowTv = this.findViewById<TextView>(R.id.txtSpeed)
-            SpeedtoShowTv.text = mySpeed.toString()
-            myUDP = mySpeed.toString() + "," + myDirection.toString()
-            sendUDP(myUDP)
-        }
-        binding.btnDecrease.setOnClickListener {
-            mySpeed -= 1
-            if (mySpeed < -10) mySpeed = -10
-            val SpeedtoShowTv = this.findViewById<TextView>(R.id.txtSpeed)
-            SpeedtoShowTv.text = mySpeed.toString()
-            myUDP = mySpeed.toString() + "," + myDirection.toString()
-            sendUDP(myUDP)
+
+        binding.gas.setOnSeekBarChangeListener( object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val progressVal = progress + 1
+
+                if( progressVal == 10){
+                    speed = 0
+                    speedText.text = "0"
+                } else if(progressVal < 10){
+                    speed = 10 - progressVal
+                    speedText.text = (10 - progressVal).toString()
+                    reverse = 1
+                } else {
+                    speed = progressVal - 10
+                    speedText.text = (progressVal - 10).toString()
+                    reverse = 0
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                //do nothing
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar != null) {
+                    seekBar.progress = 10
+                    speed = 0
+                    reverse = 0
+                    speedText.text = "0"
+                }
+            }
+
+        })
+
+        binding.btnLeft.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                direction = 0
+            }
+            if (event.action == MotionEvent.ACTION_UP) {
+                direction = 1
+            }
+            true
         }
 
-        binding.btnStop.setOnClickListener {
-            mySpeed = 0
-            myDirection = 0
-            val SpeedtoShowTv = this.findViewById<TextView>(R.id.txtSpeed)
-            SpeedtoShowTv.text = mySpeed.toString()
-            val DirtoShowTv = this.findViewById<TextView>(R.id.txtDirection)
-            DirtoShowTv.text = myDirection.toString()
-            myUDP = mySpeed.toString() + "," + myDirection.toString()
-            sendUDP(myUDP)
-        }
-        binding.btnLeft.setOnClickListener {
-            myDirection -= 1
-            if (myDirection < -5) myDirection = -5
-            val DirtoShowTv = this.findViewById<TextView>(R.id.txtDirection)
-            DirtoShowTv.text = myDirection.toString()
-            myUDP = mySpeed.toString() + "," + myDirection.toString()
-            sendUDP(myUDP)
+        binding.btnRight.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                direction = 2
+            }
+            if (event.action == MotionEvent.ACTION_UP) {
+                direction = 1
+            }
+            true
         }
 
-        binding.btnCenter.setOnClickListener {
-            myDirection = 0
-            val DirtoShowTv = this.findViewById<TextView>(R.id.txtDirection)
-            DirtoShowTv.text = myDirection.toString()
-            myUDP = mySpeed.toString() + "," + myDirection.toString()
-            sendUDP(myUDP)
-        }
-        binding.btnRight.setOnClickListener {
-            myDirection += 1
-            if (myDirection > 5) myDirection = 5
-            val DirtoShowTv = this.findViewById<TextView>(R.id.txtDirection)
-            DirtoShowTv.text = myDirection.toString()
-            myUDP = mySpeed.toString() + "," + myDirection.toString()
-            sendUDP(myUDP)
-        }
         binding.btnSettings.setOnClickListener {
             val intent = Intent(this, Settings::class.java);
             startActivity(intent)
--- app/src/main/java/com/example/controller/Settings.kt
+++ app/src/main/java/com/example/controller/Settings.kt
@@ -12,24 +12,24 @@ class Settings : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var editEnterip = mySettings.RemoteHost
-        var editEnterport = mySettings.RemotePort
+        var editEnterip = mySettings.remoteHost
+        var editEnterport = mySettings.remotePort
         setContentView(R.layout.activity_settings)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.editEnterip.setText(mySettings.RemoteHost.toString())
-        binding.editEnterport.setText(mySettings.RemotePort.toString())
+        binding.editEnterip.setText(mySettings.remoteHost.toString())
+        binding.editEnterport.setText(mySettings.remotePort.toString())
 
         binding.btnSetip.setOnClickListener {
             var finalIP = findViewById<EditText>(R.id.editEnterip)
             var textFromEditText = finalIP.text.toString() // access text this way
-            mySettings.RemoteHost = textFromEditText.toString()
+            mySettings.remoteHost = textFromEditText.toString()
             var finalPort = findViewById<EditText>(R.id.editEnterport)
             var textfromEnterport = finalPort.text.toString().toInt()
             //finalPort = "1238"
-            mySettings.RemotePort = textfromEnterport.toInt()
+            mySettings.remotePort = textfromEnterport.toInt()
 
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
